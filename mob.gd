0f9d35fc21ddeef58154a073a6be0e14145bac41
yokitann <yotan3@s.sfusd.edu>
2024-05-14 10:56:20 -0700

    FinishedVideo

--- mob.gd
+++ mob.gd
@@ -20,6 +20,6 @@ func take_damage():
 		queue_free()
 
 		const SMOKE_SCENE = preload("res://smoke_explosion/smoke_explosion.tscn")
-		var smoke =SMOKE_SCENE.instantiate()
+		var smoke = SMOKE_SCENE.instantiate()
 		get_parent().add_child(smoke)
 		smoke.global_position = global_position
