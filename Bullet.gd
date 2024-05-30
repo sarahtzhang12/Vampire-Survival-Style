09e8ad2476e827b9ff0e5192d55051d65d13c75b
yokitann <yotan3@s.sfusd.edu>
2024-05-29 18:01:45 -0700

    Fix Bullet Drop

--- Bullet.gd
+++ Bullet.gd
@@ -8,7 +8,6 @@ func _physics_process(delta):
 	
 	var direction = Vector2.RIGHT.rotated(rotation)
 	position += direction * SPEED * delta
-	 
 	travelled_distance += SPEED * delta
 	if travelled_distance > RANGE:
 		queue_free()
